d18330d9f072d39c3fb7788f03df641a2769c7ea
balani491 <115403542+balani491@users.noreply.github.com>
2023-10-04 13:23:14 +0000

    datatype

---
+++ 0_1basics/datatype.js
@@ -0,0 +1,13 @@
+"use strict";// all JS code will be treated as newer version
+// alert(3+3) we are using node js not browser
+
+//good readabilty
+//number 
+//bigint
+//sting ""
+//boolean -true/false
+//null=standalone value; it is an object
+//undefined
+//symbol=>unique
+
+console.log(typeof(2))
---
+++ 0_1basics/var.js
@@ -0,0 +1,14 @@
+const accountID=123456
+let email="balani491@gmail.com" //can be changed var can be defined by two ways i.e. let and var but there is a matter of scope that is why we use let to prevent block and functional scope problems
+var pas="12344"
+city="mumbai"
+let state;
+/*
+multi comment
+*/
+
+// accountID=2 Not allowed
+pas="1"
+city="manila"
+console.log(accountID);
+console.table([accountID,pas,city,state])
