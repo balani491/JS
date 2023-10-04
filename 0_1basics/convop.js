a9903c4440f7e050e7a78f0a7b95e1cb605f454e
balani491 <115403542+balani491@users.noreply.github.com>
2023-10-04 13:32:24 +0000

    type conversion and operations

---
+++ 0_1basics/convop.js
@@ -0,0 +1,25 @@
+// let score=true
+// console.log(typeof score)
+
+// let valueInNumber=Number(score) //type conversion
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+//"33" - 33
+//"33qabs"- NaN
+//true=1 false=0
+
+
+// let isLoggedin="hitesh"
+// let boolisLoggedin=Boolean(isLoggedin)
+// console.log(boolisLoggedin)
+
+//string -true
+//" " false
+//1 true 0-false
+
+
+let stringN="33"
+let ns=Number(stringN)
+console.log(ns)
